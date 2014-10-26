4b49c1269500c466dd1d997b48f57bf9d7cc3c5a
Admin <Admin@Admin-PC>
2014-10-26 04:05:37 -0700

    Changes in New Test Git class

---
+++ src/testgit/NewTestGit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgit;
+
+/**
+ *
+ * @author Admin
+ */
+public class NewTestGit 
+{
+ 
+    public static void main(String [] h )
+    {
+        System.out.println("Hello NEw print in here");
+    }
+}
