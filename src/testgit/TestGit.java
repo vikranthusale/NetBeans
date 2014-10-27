2f2e1b34d043921c38986c1c20a9614de3019d08
Kamod <Kamod@Kamod-pc>
2014-10-27 13:00:12 +0530

    Chetan Changed

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -18,7 +18,7 @@ public class TestGit {
     public static void main(String[] args) 
     {
         // TODO code application logic here
-        System.out.println("Hello Git Things have been TTTTTTTTTT changed ........");
+        System.out.println("Hello Git Things have been  changed ........");
     }
     
 }
